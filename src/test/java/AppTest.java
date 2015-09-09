be388f373098fe33b35a430ecc773feba7ad3293
Ashley Sullins & Ariel Spear & Colleen Minor & Kallen Millner & Stephany Garcia <student@epicodus.com>
2015-09-09 10:43:53 -0700

    Add inital files

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,27 @@
+import org.fluentlenium.adapter.FluentTest;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import static org.fluentlenium.core.filter.FilterConstructor.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest extends FluentTest {
+  public WebDriver webDriver = new HtmlUnitDriver();
+
+  @Override
+  public WebDriver getDefaultDriver() {
+      return webDriver;
+  }
+
+  @ClassRule
+  public static ServerRule server = new ServerRule();
+
+  @Rule
+  public DataBaseRule database = new DataBaseRule();
+
+  @Test
+  // firsttesthere!
+}
---
+++ src/test/java/DataBaseRule.java
@@ -0,0 +1,19 @@
+import org.junit.rules.ExternalResource;
+import org.sql2o.*;
+
+public class DataBaseRule extends ExternalResource {
+
+  protected void before() {
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/gems_test", null, null);
+   }
+
+  protected void after() {
+    try(Connection con = DB.sql2o.open()) {
+      // WILL BE NAMING THESE BASED ON USER-GENERATED TABLE
+      // String deleteTasksQuery = "DELETE FROM tasks *;";
+      // String deleteCategoriesQuery = "DELETE FROM categories *;";
+      // con.createQuery(deleteTasksQuery).executeUpdate();
+      // con.createQuery(deleteCategoriesQuery).executeUpdate();
+    }
+  }
+}
